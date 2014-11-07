8effc2d48e87fc8f6d3a8ab8b9ae785562ddea01
Venkata Raju <Raju7003@gmail.com>
2014-11-07 08:02:05 +0530

    Reduced delay time

--- JavaCodeGeeks.com/JavaCodeGeeks.com-ClosePopup.user.js
+++ JavaCodeGeeks.com/JavaCodeGeeks.com-ClosePopup.user.js
@@ -8,13 +8,19 @@
 // @match       http://*.javacodegeeks.com/*
 // @match       http://webcodegeeks.com/*
 // @match       http://*.webcodegeeks.com/*
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
-(function delayedFun(count)
+
+/** Version History
+    1.01  : 1. Updated description (Added WebCodeGeeks.com)
+            2. Reduced delay time   
+*/
+
+(function closePopup(attemptNum)
 {
   //FIX ME: This function is being called twice.
   var el = document.querySelector("a.fancybox-close");
-  el ? el.click() : (count < 20) ? setTimeout(function() { delayedFun(count + 1); }, 4000) : "dummy";
+  el ? el.click() : (attemptNum < 17) ? setTimeout(function() closePopup(attemptNum + 1), 600) : "dummy";
 })(0);
